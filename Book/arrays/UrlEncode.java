1f33a78f37463431fd61286ccbc28acfd1a7f2bb
AdrianVarelaG <cesarac66@hotmail.com>
2020-08-22 20:50:07 +0000

    UrlEncode Exercise

---
+++ Book/arrays/UrlEncode.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class UrlEncode {
+    public static void main(String[] args) {
+        UrlEncode process = new UrlEncode();
+    }
+
+    public char[] encodeURl(String input, Integer size) {
+        char[] inputArray = input.toCharArray();
+    }
+
+}
